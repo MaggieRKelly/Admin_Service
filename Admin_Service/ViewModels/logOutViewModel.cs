5348187456e60d7cc49b6850f870a534e2dce6ec
Maggie Kelly <maggie.kelly8812@gmail.com>
2017-12-05 15:33:06 +0000

    A bit more

---
+++ Admin_Service/ViewModels/logOutViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admin_Service.ViewModels
+{
+    public class logOutViewModel
+    {
+        [Display(Name = "LogOut")]
+        public bool Logout { get; set; }
+    }
+}
